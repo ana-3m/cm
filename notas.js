2bbcbd53db8c4644b1e3bf03ab80c62f5a22054c
r-coimbra <rafaelcoimbragomes27@gmail.com>
2025-04-22 19:19:41 +0100

    bolas a alterar após click

--- notas.js
+++ notas.js
@@ -1,10 +1,13 @@
 import { getGreenSquares } from './detetor.js';
 
+let numCirc; //Variavel com o numero do círculo a ser desenhado
+let mudar;
 const drumSet = new Image();
 drumSet.src = "img/drumSetDisplay.png";
 
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
+
 
 function resizeCanvas() {
     canvas.width = window.innerWidth;
@@ -15,12 +18,21 @@ window.addEventListener("resize", resizeCanvas);
 resizeCanvas();
 
 
+canvas.addEventListener("touchstart",(event) => { 
+    if (calculateDistance(event.touches[0].clientX, event.touches[0].clientY, greenSquares[numCirc].x, greenSquares[numCirc].y) < greenSquares[numCirc].radius) {
+        console.log("Clicked on circle", numCirc);
+        mudar = true;
+    }
+    update();
+});
+
 let greenSquares = []; //Array para guardar a loc dos quadrados verdes
-const maxRadius = 40; // Raio máximo dos círculos
+const maxRadius = 25; // Raio máximo dos círculos
 const growthRate = 0.75; // Taxa de crescimento dos círculos
 
-function initializeGreenSquares() {
+async function initializeGreenSquares() {
     greenSquares = getGreenSquares().map(square => ({ ...square, radius: 0 }));
+    numCirc = await getRandomInt(0, greenSquares.length - 1);
 }
 
 function update() {
@@ -31,15 +43,21 @@ function update() {
     });
 }
 
-function draw() {
+async function draw() {
+    console.log(greenSquares);
     ctx.drawImage(drumSet, 0, 0, innerWidth, innerHeight);
     ctx.fillStyle = "red";
     // Desenhar os círculos vermelhos a crescer
-   greenSquares.forEach(square => {
-        ctx.beginPath();
-        ctx.arc(square.x, square.y, square.radius, 0, Math.PI * 2);
-        ctx.fill();
-    });
+    //let random = await getRandomInt(0, greenSquares.length - 1);
+    if(mudar) {
+        numCirc = await getRandomInt(0, greenSquares.length - 1);
+        console.log("I am inside the click function", numCirc);
+        mudar = false;
+    }
+    console.log(numCirc);
+    ctx.beginPath();
+    ctx.arc(greenSquares[numCirc].x, greenSquares[numCirc].y, greenSquares[numCirc].radius, 0, Math.PI * 2);
+    ctx.fill();
 }
 
 function gameLoop() {
@@ -47,6 +65,18 @@ function gameLoop() {
     draw();
     requestAnimationFrame(gameLoop);
 }
+
+async function getRandomInt(min, max) {
+
+    let random = await Math.round(Math.random() * (max - min) + min);
+    return random;
+}
+
+function calculateDistance(x1, y1, x2, y2) {
+    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+}
+
+
 
 // Initialize green squares after a short delay to ensure detection is complete
 setTimeout(() => {
